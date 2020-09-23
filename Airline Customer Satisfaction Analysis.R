a2305ee2168ed3bf55a32a0033243fd29fbdd67c
Urvi Mistry <47509098+UrviMistry@users.noreply.github.com>
2020-09-23 19:18:17 -0400

    Update Airline Customer Satisfaction Analysis.R

--- Airline Customer Satisfaction Analysis.R
+++ Airline Customer Satisfaction Analysis.R
@@ -1,218 +1,119 @@
 data=read.csv("C:\\Users\\Urvi Mistry\\Desktop\\SS.csv", header=TRUE)   # Naming the file data
 
-
 # Renaming Columns 
-
 colnames(data)<-c("satisfaction","airline_status","age","gender","price_sensitivity","year_of_first_flight","no_of_flights_per_annum",
                   "percent_of_flights_with_other_airlines","type_of_travel","no_of_loyalty_cards","shopping_amount_at_airport",
                   "eat_drink_at_airport","class","day_of_month","flight_date","airline_code","airline_name","origin_city",
                   "origin_state","destination_city","destination_state","schedule_departure_hour","departure_delay_in_minutes",
                   "arrival_delay_in_minutes","flight_cancelled","flight_time_in_minutes",
                   "flight_distance","arrival_delay_greater_than_5_minutes") 
-
 data$satisfaction<-substr(data$satisfaction,0,4) # treating 4.00.34 properly
-
 str(data)
-
 data$satisfaction<-as.numeric(data$satisfaction)
-
 data$cust_satisfaction<-as.factor(data$cust_satisfaction)
-
 cust_satisfaction=replicate(length(data$satisfaction), "nil")
 cust_satisfaction[data$satisfaction >= 3]="1"
 cust_satisfaction[data$satisfaction < 3]="0"
 data$cust_satisfaction =cust_satisfaction
 
 # Finding null values
-
 colSums(is.na(data))
-
 data_c<-subset(data,flight_cancelled=="Yes")
-
 head(data_c)
-
 colSums(is.na(data_c))
-
 dim(data_c)
 
-## Replacing NA's in  by mean
-
-
+# Replacing NA's in departure dealy in minutes by mean
 data_c$departure_delay_in_minutes[is.na(data_c$departure_delay_in_minutes)]<-round(mean(data_c$departure_delay_in_minutes,na.rm=TRUE))
 
 # Replacing NAs by 0
-
 data_c$flight_time_in_minutes <- replace(data_c$flight_time_in_minutes, is.na(data_c$flight_time_in_minutes),0)
-
 data_c$arrival_delay_in_minutes <- replace(data_c$arrival_delay_in_minutes, is.na(data_c$arrival_delay_in_minutes),0)
-
 data_m<-subset(data,flight_cancelled=="No")
-
 colSums(is.na(data_m))
 
-## Replacing NA's in Arrival_delay_in_minutes by mean
-
-
-
+# Replacing NA's in Arrival_delay_in_minutes by mean
 data_m$arrival_delay_in_minutes[is.na(data_m$arrival_delay_in_minutes)]<-round(mean(data_m$arrival_delay_in_minutes,na.rm=TRUE))
 
-## Replacing NA's in fligt_time_in_minutes by mean
-
+# Replacing NA's in fligt_time_in_minutes by mean
 data_m$flight_time_in_minutes[is.na(data_m$flight_time_in_minutes)]<-round(mean(data_m$flight_time_in_minutes,na.rm=TRUE))
-
 data_s<-subset(data_m,airline_name=="Southeast Airlines Co. ")
-
 dim(data_s)
-
 colSums(is.na(data_s))
-
 data_a<-subset(data_m, airline_name!="Southeast Airlines Co. " )
-
 dim(data_a)
 
 # Plots
-
 library(ggplot2)
 
-ggplot(data_s,aes(x=age,y=satisfaction))+geom_count()+
-  stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
+ggplot(data_s,aes(x=age,y=satisfaction))+geom_count()+stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
 
-ggplot(data_a,aes(x=age,y=satisfaction))+geom_count()+
-  stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
+ggplot(data_a,aes(x=age,y=satisfaction))+geom_count()+stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
 
-ggplot(data_s,aes(x=gender,y=satisfaction))+geom_count()+
-  stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
+ggplot(data_s,aes(x=gender,y=satisfaction))+geom_count()+stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
 
-ggplot(data_a,aes(x=gender,y=satisfaction))+geom_count()+
-  stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
+ggplot(data_a,aes(x=gender,y=satisfaction))+geom_count()+stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
-ggplot(data_s,aes(x=type_of_travel,y=satisfaction))+geom_count()+
-  stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
+ggplot(data_s,aes(x=type_of_travel,y=satisfaction))+geom_count()+stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
 
-ggplot(data_a,aes(x=type_of_travel,y=satisfaction))+geom_count()+
-  stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
+ggplot(data_a,aes(x=type_of_travel,y=satisfaction))+geom_count()+stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
 
-ggplot(data_s,aes(x=class,y=satisfaction))+geom_count()+
-  stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
+ggplot(data_s,aes(x=class,y=satisfaction))+geom_count()+stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
 
-ggplot(data_a,aes(x=class,y=satisfaction))+geom_count()+
-  stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
+ggplot(data_a,aes(x=class,y=satisfaction))+geom_count()+ stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
 
 # Satisfaction Vs Airline Status
+ggplot(data_s,aes(x=airline_status,y=satisfaction))+geom_count()+ stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
 
-ggplot(data_s,aes(x=airline_status,y=satisfaction))+geom_count()+
-  stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
-
-ggplot(data_a,aes(x=airline_status,y=satisfaction))+geom_count()+ 
-  stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
+ggplot(data_a,aes(x=airline_status,y=satisfaction))+geom_count()+ stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
 
 # Satisfaction Vs No of flights per annum
+ggplot(data_s,aes(x=no_of_flights_per_annum,y=satisfaction))+geom_count()+stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
 
-ggplot(data_s,aes(x=no_of_flights_per_annum,y=satisfaction))+geom_count()+
-  stat_summary(aes(y =data_s$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
-
-ggplot(data_a,aes(x=no_of_flights_per_annum,y=satisfaction))+geom_count()+
-  stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
+ggplot(data_a,aes(x=no_of_flights_per_annum,y=satisfaction))+geom_count()+stat_summary(aes(y =data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
 # satisfaction Vs year of first flight
 
 data_s$year_of_first_flight<-as.factor(data_s$year_of_first_flight)
 data_a$year_of_first_flight<-as.factor(data_a$year_of_first_flight)
 
-ggplot(data_s,aes(x=year_of_first_flight,y=satisfaction))+geom_count(aes(color = ..n..))+
-  stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
+ggplot(data_s,aes(x=year_of_first_flight,y=satisfaction))+geom_count(aes(color = ..n..))+stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
 
 ggplot(data_a,aes(x=year_of_first_flight,y=satisfaction))+geom_count(aes(color = ..n..))+
   stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
 
 # Satisfaction Vs shopping amount at AIRPORT
+ggplot(data_s,aes(x=shopping_amount_at_airport,y=satisfaction))+geom_count()+stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
-ggplot(data_s,aes(x=shopping_amount_at_airport,y=satisfaction))+geom_count()+
-  stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
+ggplot(data_a,aes(x=shopping_amount_at_airport,y=satisfaction))+geom_count()+stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
 
+ggplot(data_s,aes(x=eat_drink_at_airport,y=satisfaction))+geom_count()+stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
-ggplot(data_a,aes(x=shopping_amount_at_airport,y=satisfaction))+geom_count()+
-  stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
+ggplot(data_a,aes(x=eat_drink_at_airport,y=satisfaction))+geom_count()+stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
 
+ggplot(data_s,aes(x=departure_delay_in_minutes,y=satisfaction))+geom_count()+stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
+ggplot(data_a,aes(x=departure_delay_in_minutes,y=satisfaction))+geom_count()+stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
 
+ggplot(data_s,aes(x=arrival_delay_in_minutes,y=satisfaction))+geom_count()+stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
+ggplot(data_a,aes(x=arrival_delay_in_minutes,y=satisfaction))+geom_count()+stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
 
-ggplot(data_s,aes(x=eat_drink_at_airport,y=satisfaction))+geom_count()+
-  stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
+ggplot(data_s,aes(x=flight_time_in_minutes,y=satisfaction))+geom_count(aes(color = ..n..))+stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
+ggplot(data_a,aes(x=flight_time_in_minutes,y=satisfaction))+geom_count(aes(color = ..n..))+stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
 
+ggplot(data_s,aes(x=flight_distance,y=satisfaction))+geom_count()+stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
+ggplot(data_a,aes(x=flight_distance,y=satisfaction))+geom_count()+stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
 
-ggplot(data_a,aes(x=eat_drink_at_airport,y=satisfaction))+geom_count()+
-  stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
+ggplot(data_s,aes(x=arrival_delay_greater_than_5_minutes,y=satisfaction))+geom_count(aes(color = ..n..))+stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
 
+ggplot(data_a,aes(x=arrival_delay_greater_than_5_minutes,y=satisfaction))+geom_count(aes(color = ..n..))+stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
 
+ggplot(data_s,aes(x=day_of_month,y=satisfaction))+geom_count(aes(color = ..n..))+stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
 
-
-
-
-ggplot(data_s,aes(x=departure_delay_in_minutes,y=satisfaction))+geom_count()+
-  stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
-
-
-ggplot(data_a,aes(x=departure_delay_in_minutes,y=satisfaction))+geom_count()+
-  stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
-
-
-
-
-
-ggplot(data_s,aes(x=arrival_delay_in_minutes,y=satisfaction))+geom_count()+
-  stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
-
-
-ggplot(data_a,aes(x=arrival_delay_in_minutes,y=satisfaction))+geom_count()+
-  stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
-
-
-
-
-
-ggplot(data_s,aes(x=flight_time_in_minutes,y=satisfaction))+geom_count(aes(color = ..n..))+
-  stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
-
-
-ggplot(data_a,aes(x=flight_time_in_minutes,y=satisfaction))+geom_count(aes(color = ..n..))+
-  stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
-
-
-
-
-
-ggplot(data_s,aes(x=flight_distance,y=satisfaction))+geom_count()+
-  stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="point",group=1)
-
-
-ggplot(data_a,aes(x=flight_distance,y=satisfaction))+geom_count()+
-  stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="point",group=1)
-
-
-
-
-
-ggplot(data_s,aes(x=arrival_delay_greater_than_5_minutes,y=satisfaction))+geom_count(aes(color = ..n..))+
-  stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
-
-
-ggplot(data_a,aes(x=arrival_delay_greater_than_5_minutes,y=satisfaction))+geom_count(aes(color = ..n..))+
-  stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
-
-
-
-
-
-ggplot(data_s,aes(x=day_of_month,y=satisfaction))+geom_count(aes(color = ..n..))+
-  stat_summary(aes(y=data_s$satisfaction ,group=1), fun.y=mean, colour="red", geom="line",group=1)
-
-
-ggplot(data_a,aes(x=day_of_month,y=satisfaction))+geom_count(aes(color = ..n..))+
-  stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
+ggplot(data_a,aes(x=day_of_month,y=satisfaction))+geom_count(aes(color = ..n..))+stat_summary(aes(y=data_a$satisfaction ,group=1), fun.y=mean, colour="blue", geom="line",group=1)
 
 data_s$cust_satisfaction <-as.factor(data_s$cust_satisfaction)
 data_a$cust_satisfaction <-as.factor(data_a$cust_satisfaction)
@@ -226,7 +127,6 @@ b1<-b1+geom_bar(stat="identity",fill="black",width=0.2)
 b1<-b1+xlab("Cust satisfaction")+ylab("NUmber of satisfied customers")+ggtitle("1=Satisfied cutomers,0=not satisfied customer")
 b1
 
-
 b0<-data.frame(table(data_a$cust_satisfaction))
 b0
 
@@ -239,7 +139,6 @@ b1
 #Linear Regression On Southeast:
 
 class(s)
-
 lm1<-lm(satisfaction~age+airline_status+price_sensitivity+year_of_first_flight+no_of_flights_per_annum
         +percent_of_flights_with_other_airlines+type_of_travel+no_of_loyalty_cards+shopping_amount_at_airport
         +eat_drink_at_airport+class+day_of_month+flight_date+origin_city+origin_state+destination_city+destination_state
@@ -249,7 +148,6 @@ lm1<-lm(satisfaction~age+airline_status+price_sensitivity+year_of_first_flight+n
 summary(lm1)
 
 #Taing significant P values to make a new model
-
 final_lm<-lm(satisfaction~age+airline_status+no_of_flights_per_annum+type_of_travel+eat_drink_at_airport+
                class+schedule_departure_hour+arrival_delay_greater_than_5_minutes,
              data=data_s)
@@ -259,7 +157,6 @@ summary(final_lm)
 # But we see that Adjusted R square has gone up, which indicates new model is better and we can discard other variables..
 
 # Logistic
-
 logit.m<-glm(cust_satisfaction~age+airline_status+price_sensitivity+year_of_first_flight+no_of_flights_per_annum
              +percent_of_flights_with_other_airlines+type_of_travel+no_of_loyalty_cards+shopping_amount_at_airport
              +eat_drink_at_airport+class+day_of_month
@@ -274,7 +171,6 @@ summary(logit.m)
 # doing against the null model (a model with only the intercept). The wider this gap, the better.
 
 anova(logit.m,test="Chisq")
-
 anova(logit.m,test="Chisq")
 
 # Adding Age, airline_status and type_of_travel significantly reduces the residual deviance. 
@@ -294,8 +190,6 @@ summary(logit.m)
 anova(logit.final,test="Chisq")
 
 # From both linear and logistic regression we get the sam variables which are significant so now we will use only those variables for prediction
-
-
 nrow(data_s)
 
 test<-data_s[7000:9000,]
@@ -309,7 +203,6 @@ misClasificError <- mean(fitted.results != data_s$cust_satisfaction)
 print(paste('Accuracy',1-misClasificError))
 
 # Finding area under the curve
-
 library(ROCR)
 
 p<- predict(logit.m,newdata=subset(test,select=c(2,3,4,5,6,7,8,9,10,11,12,13,14,22,23,24,26,27,28)),type='response')
@@ -346,12 +239,9 @@ set.seed(3233)
 mysvm<-train(cust_satisfaction~age+airline_status+no_of_flights_per_annum+type_of_travel+departure_delay_in_minutes+
                arrival_delay_in_minutes+arrival_delay_greater_than_5_minutes,
              data=training, 
-             method="svmLinear"
-)
-
+             method="svmLinear")
 
 summary(mysvm)
-
 mysvm
 
 # SVM class method
